83175f3bf16d3c5644feb4a5177a5f3e8db97eea
Balázs Indig <dlazesz@users.noreply.github.com>
2026-04-15 20:30:46 +0200

    Completely disable Shibboleth when not neededed

--- conf/entrypoint.sh
+++ conf/entrypoint.sh
@@ -17,24 +17,29 @@ if [ $# -eq 1 ]; then
     sed -i "s#SERVER_ALIAS_PLACEHOLDER#${SERVER_ALIAS}#" /etc/apache2/sites-enabled/000-default.conf
     sed -i "s#SERVER_NAME_PLACEHOLDER#${SERVER_NAME}#" /etc/shibboleth/shibboleth2.xml
     sed -i "s#CITATION_LINK_PLACEHOLDER#${CITATION_LINK}#" /var/www/crystal/bundle.js
-    # ENV variables take precedence
-    if [[ -n "${PRIVATE_KEY}" ]] || [[ ! -s "/etc/shibboleth/sp.for.eduid.service.hu-key.crt" ]]; then
+    # ENV variables take precedence (note: The whitespace check prevents echo from adding a new line, which would make the file non-empty)
+    # If ENV variable set with non-whitespace content or (the file is missing, empty, or contain whitespace characters only)
+    if [[ "${PRIVATE_KEY}" =~ [^[:space:]] ]] || ! grep -q '[^[:space:]]' "/etc/shibboleth/sp.for.eduid.service.hu-key.crt" 2>/dev/null; then
         echo "${PRIVATE_KEY}" > /etc/shibboleth/sp.for.eduid.service.hu-key.crt
     fi
     chmod 644 /etc/shibboleth/sp.for.eduid.service.hu-key.crt
-    if [[ -n "${PUBLIC_KEY}" ]] || [[ ! -s "/etc/shibboleth/sp.for.eduid.service.hu-cert.crt" ]]; then
+    if [[ "${PUBLIC_KEY}" =~ [^[:space:]] ]] || ! grep -q '[^[:space:]]' "/etc/shibboleth/sp.for.eduid.service.hu-cert.crt" 2>/dev/null; then
         echo "${PUBLIC_KEY}" > /etc/shibboleth/sp.for.eduid.service.hu-cert.crt
     fi
     chmod 644 /etc/shibboleth/sp.for.eduid.service.hu-cert.crt
-    if [[ -n "${HTACCESS}" ]] || [[ ! -s "/var/www/.htaccess" ]]; then
+    if [[ "${HTACCESS}" =~ [^[:space:]] ]] || ! grep -q '[^[:space:]]' "/var/www/.htaccess" 2>/dev/null; then
         echo "${HTACCESS}" > /var/www/.htaccess
     fi
-    if [[ -n "${HTPASSWD}" ]] || [[ ! -s "/var/lib/bonito/htpasswd" ]]; then
+    if [[ "${HTPASSWD}" =~ [^[:space:]] ]] || ! grep -q '[^[:space:]]' "/var/lib/bonito/htpasswd" 2>/dev/null; then
         echo "${HTPASSWD}" > /var/lib/bonito/htpasswd
     fi
     # Must be started after apache (only if cert and key are not empty)
-    if [[ -s "/etc/shibboleth/sp.for.eduid.service.hu-cert.crt" ]]; then
+    if grep -q '[^[:space:]]' "/etc/shibboleth/sp.for.eduid.service.hu-cert.crt" 2>/dev/null; then
         (sleep 5 && service shibd start && echo 'Shibd started.' || exit 1) &
+    else
+        # Skipping shibd start and disabling apache module and config to avoid degrading over time
+        a2dismod shib
+        a2disconf shib
     fi
     /usr/sbin/apache2ctl -D FOREGROUND
 else
